6f472d97e4163f6f3d606778387da28c236a3f12
n8houl <n8houl@gmail.com>
2017-10-03 16:53:41 -0400

    Finished q5
    Still need to test with other images

--- q5.py
+++ q5.py
@@ -5,19 +5,24 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as img
 import numpy as np
+
+import math
 """
     GENERAL
         - Functions for reading / writing image using matplotlib
         - Manual conversion from rgb to grayscale being performed
 """
-def get_img(fname):
+def get_img(fname, grayscale=0):
     print("Reading in image")
     I = img.imread(fname)
     new_img = []
     for row in I:
         new_row = []
         for elem in row:
-            new_row.append(elem)
+            if(grayscale == 1):
+                new_row.append(elem)
+            else:
+                new_row.append(elem[0])
         new_img.append(new_row)
     return new_img
 
@@ -30,6 +35,10 @@ def put_img(I, fname):
             new_row.append([row[i], row[i], row[i], 1.0])
         new_img.append(new_row)
     img.imsave(fname, new_img)
+
+def fput_img(I, fname, id, ext):
+    filename = "" + fname + "_" + id + "." + ext
+    put_img(I, filename)
 
 """
     END GENERAL
@@ -109,10 +118,52 @@ def quantize_img(I):
 """
     PART 2 CODE
 """
+# greyscale clipping function
+def clipVFunction(i, a, b, beta):
+    ret = -1
+    if i < a and i >= 0:
+        ret = 0
+    elif i < b and i >= a:
+        ret = beta * (i - a)
+    else:
+        ret = beta * (b - a)
+    return ret
 
+#perform clipping operation on each pixel in image
+def clip(I, a, b, beta):
+    print("Performing clipping gradation")
+    I = quantize_img(I)
+    new_img = I
+    print("Applying clip function")
+    for i in range(len(I)):
+        for j in range(len(I[0])):
+            new_img[i][j] = clipVFunction(I[i][j], a, b, beta)
+    return new_img
 
 """
     END PART 2 CODE
+"""
+
+def rangeCompressionFunction(i, c):
+    return c * math.log10(1 + i)
+
+def rangeCompression(I, c):
+    print("Performing log10 range compression with c=%d" % c)
+    I = quantize_img(I)
+    new_img = I
+    print("Applying log10 range compression function")
+    for i in range(len(I)):
+        for j in range(len(I[0])):
+            new_img[i][j] = rangeCompressionFunction(I[i][j], c)
+
+    return new_img
+"""
+    PART 3 CODE
+"""
+
+
+"""
+    END PART 3 CODE
 """
 
 
@@ -120,12 +171,23 @@ def quantize_img(I):
     Running code
 """
 #PART 1
-I = get_img('img/moo2.png')
+I = get_img('img/moo2.png', grayscale=1)
 I = quantize_img(I)
 plot_hist(I)
 I = hist_eq(I)
 plot_hist(I)
 
-put_img(I, 'img/q5/moo2.png')
+put_img(I, 'img/q5/moo2_eq.png')
 
 #PART 2
+# this is all set up, just need to find a good image to use it on
+I = get_img('img/input3.png')
+I = clip(I, 50, 150, 2)
+put_img(I, 'img/q5/input3_clipped.png')
+
+#PART 3
+c_values = [1, 10, 100, 1000]
+I = get_img('img/input3.png')
+for value in c_values:
+    img1 = rangeCompression(I, value)
+    fput_img(img1, 'img/q5/input3', '%d' % value, 'png')
